3b0a7eeda61bf5733cae18c7016be8d36b0b2a4f
javramnun <javramnun@alum.us.es>
2024-02-20 13:45:04 +0100

    Group Mandatory 7)

---
+++ src/main/java/acme/entities/Banner.java
@@ -0,0 +1,56 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Atributes ---------------------------------------------------------------
+
+	@NotNull
+	@Past
+	private Date				instationUpdateMoment;
+
+	//////////////////////////
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				finishTime;
+	////////////////////////// REVISAR
+
+	@NotNull
+	@URL
+	private String				linkPicture;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				slogan;
+
+	@NotNull
+	@URL
+	private String				linkDocument;
+}
