a61c629d419461649cce61b7cff3af42eb208644
SoniaRM <sonrusmor@alum.us.es>
2024-03-05 19:36:31 +0100

    D02 - 22) Administrator Dashboard

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,39 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdministratorDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	Map<String, Integer>		totalPrincipalEachRole;
+	Double						ratioOfNoticesWithEmailAndLink;
+	Double						ratioOfCriticalObjectives;
+
+	Double						avgValueRisk;
+	Double						minValueRisk;
+	Double						maxValueRisk;
+	Double						devValueRisk;
+
+	Double						avgClaimsOverLastTenWeeks;
+	Double						minClaimsOverLastTenWeeks;
+	Double						maxClaimsOverLastTenWeeks;
+	Double						devClaimsOverLastTenWeeks;
+
+	// Derived attributes -----------------------------------------------------
+
+	Double						ratioOfNotCriticalObjectives;
+
+	// Relationships ----------------------------------------------------------
+}
