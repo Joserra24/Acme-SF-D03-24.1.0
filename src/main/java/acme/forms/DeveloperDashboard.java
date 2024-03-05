efbee6d73cb91a11daf6c82a7140df0e25c4e52a
Joserra24 <josbanbot@alum.us.es>
2024-03-05 19:16:27 +0100

    Student3 - Mandatory D02 - 4 Developer Dashboard

---
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -0,0 +1,30 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeveloperDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	Integer						totalTrainingModulesWithUpdateMoment;
+	Integer						totalTrainingSessionsWithLink;
+
+	Double						avgTimeTrainigModule;
+	Double						devTimeTrainigModule;
+	Double						minTimeTrainigModule;
+	Double						maxTimeTrainigModule;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
